e488f2e702a7e05e9e1a17126ec8d0d34751ee43
Ogu Chinanu Mmaduabuchi <mmaduabuchiogu4@gmail.com>
2025-09-09 05:03:22 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@
 
 export const CEO_EMAIL = 'somicwater@proton.me'
 
-export const PRICE_PER_BAG = 220  // default price per bag used in calculations
+export const PRICE_PER_BAG = 210  // default price per bag used in calculations
